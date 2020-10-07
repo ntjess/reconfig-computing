c50ee499f9a7d870f67e9bdd6f5b2cce0ba31369
Nathan Jessurun <ntjess@rocketmail.com>
2020-10-07 01:31:40 -0400

    debugging for go extra credit lab 3

--- lab3/provided_code/sw/main.cpp
+++ lab3/provided_code/sw/main.cpp
@@ -22,21 +22,25 @@ using namespace std;
 #define C_RESULT_ADDR 2
 #define C_DONE_ADDR   3
 
+#define MAX_TIMEOUT   2000
+
 unsigned int calcFib(int n) {
   if (n == 0) return 0;
   else if (n == 1) return 1;
   else return calcFib(n-1) + calcFib(n-2);
 }
 
-void waitWhile(unsigned int doneCmp, Board *board, unsigned int iterNum) {
-  unsigned int timeoutCnt = 0, done = 0;
-  while (done == doneCmp && timeoutCnt < UINT_MAX) {
-    board->read(&done, C_DONE_ADDR, 1);
+uint waitWhile(unsigned int cmp, uint addr, Board *board) {
+  uint timeoutCnt = 0;
+  uint readVal = cmp;
+  while (readVal == cmp && timeoutCnt < MAX_TIMEOUT) {
+    board->read(&readVal, addr, 1);
     timeoutCnt++;
   }
-  if (timeoutCnt == UINT_MAX) {
-    cout << "Timeout error! (iteration " << iterNum << ")" << endl;
+  if (timeoutCnt == MAX_TIMEOUT) {
+    cout << "Timeout error! (timeout = " << timeoutCnt << ")" << endl << flush;
   }
+  return timeoutCnt;
 }
 
 int main(int argc, char* argv[]) {
@@ -65,20 +69,23 @@ int main(int argc, char* argv[]) {
 
   unsigned int go = 0, result = 0;
   board->write(&go, C_GO_ADDR, 1);
-
+  uint waitCnt = 0;
   for (unsigned ii = 1; ii <= 30; ii++) {
     board->write(&ii, C_N_ADDR, 1);
     go = 1;
     board->write(&go, C_GO_ADDR, 1);
-    waitWhile(0, board, ii);
+    waitWhile(0, C_DONE_ADDR, board);
 
     board->read(&result, C_RESULT_ADDR, 1);
-    go = 0;
-    board->write(&go, C_GO_ADDR, 1);
+    waitCnt = waitWhile(1, C_GO_ADDR, board);
+    if (waitCnt == MAX_TIMEOUT) {
+      // Device failed to update 'go' on its own
+      go = 0;
+      board->write(&go, C_GO_ADDR, 1);
+    }
+    waitWhile(1, C_DONE_ADDR, board);
 
     cout << ii << ": HW = " << result << ", SW = " << calcFib(ii) << endl;
-
-    waitWhile(1, board, ii);
   }
   
   return 1;
